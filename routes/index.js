ed93e818d6f644c4e819402cb01880ca140d6a4c
Mayzer <Tolik.Korzunov@yandex.ru>
2025-12-17 11:32:18 +0300

    Added follow-controller

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const { UserController, PostController, CommentController, LikeController } = require('../controllers');
+const { UserController, PostController, CommentController, LikeController, FollowController } = require('../controllers');
 const authenticateToken = require('../middleware/auth');
 
 const uploadDestination = 'uploads'
@@ -38,5 +38,9 @@ router.delete('/comments/:id', authenticateToken, CommentController.deleteCommen
 // Роуты лайков
 router.post('/likes', authenticateToken, LikeController.likePost)
 router.delete('/likes/:id', authenticateToken, LikeController.unlikePost)
+
+// Роуты подписок
+router.post('/follow', authenticateToken, FollowController.followUser)
+router.delete('/unfollow/:id', authenticateToken, FollowController.unfollowUser)
 
 module.exports = router;
